a2b01d9460e5a5242f4da959752760951c910702
Casey Deen <deen@mpia.de>
2015-10-21 11:06:13 +0200

    added Installation Instructions

---
+++ MoogSource/recompileMoogStokes_forDan.sh
@@ -0,0 +1,5 @@
+unset F77
+unset LDFLAGS
+unset F2C
+f2py --f77flags=-fcheck=all --fcompiler=gnu95 -c MoogStokesPy.pyf MoogStokessilent.f  -L/usr/lib/ -llapack Begin.f Infile.f Getasci.f Nansi.f Getcount.f Synth.f Synspec.f Finish.f Opacit.f OpacHydrogen.f OpacHelium.f Opacmetals.f Opacscat.f Opaccouls.f Nearly.f Eqlib.f Partnew.f Cdcalc.f Jexpint.f Linlimit.f Prinfo.f Sunder.f Gammabark.f Rinteg.f Batom.f Bmolec.f Inlines.f Params.f Putasci.f Taukap.f Voigt.f Discov.f Inmodel.f Partfn.f Damping.f Ucalc.f Invert.f Trudamp.f Lineinfo.f SynStokes.f Spline.f SplineDriver.f WaveGrid.f Curfit.f CalcGeom.f DELOQuad.f CalcOpacities.f ComplexVoigt.f StokesTrace.f StokesDipStick.f
+cp MoogStokesPy.so ../MoogTools/
