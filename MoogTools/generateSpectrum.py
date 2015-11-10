1caf52b4a84a9204545d1d3eab2c92cad68e6b3b
Casey Deen <deen@mpia.de>
2015-11-10 12:40:29 +0100

    blah

--- MoogTools/generateSpectrum.py
+++ MoogTools/generateSpectrum.py
@@ -4,5 +4,5 @@ import sys
 
 moogPyConfigFile = sys.argv[1]
 flavor = sys.argv[2]
-Moog = MoogTools.MoogStokes(moogPyConfigFile, moogInstance='Alpha',  progressBar = True)
+Moog = MoogTools.MoogStokes(moogPyConfigFile, fileBase=flavor, moogInstance='Alpha',  progressBar = True)
 Moog.run(saveRaw=True)
